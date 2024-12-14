1a0b2b3f1b06806b82976ae54dfef43e79d01659
Kirill <kirillzavitov@yandex.ru>
2024-12-14 20:18:18 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import random
 import sys
 
-from PyQt6 import uic
 from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QMainWindow, QApplication
 
+from UI import Ui_MainWindow
 
-class MyWidget(QMainWindow):
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
 
         self.w, self.h = 800, 452
         self.do_paint = False
@@ -30,8 +31,8 @@ class MyWidget(QMainWindow):
         self.update()
 
     def draw(self, qp):
-        qp.setBrush(QColor('yellow'))
         for i in range(random.randint(5, 15)):
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             r = random.randint(10, 50)
             qp.drawEllipse(QPoint(random.randint(0, self.w), random.randint(0, self.h)), r, r)
 
